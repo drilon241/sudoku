9283128e9c6f6d0e81da3bc2ad20267cf2dd64e1
Drilon Mecuku <driloni92@hotmail.com>
2023-01-11 17:46:28 +0000

    add first test

---
+++ cypress/e2e/loads.cy.ts
@@ -0,0 +1,7 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('/')
+    cy.contains('.status__time', '00:03')
+    cy.get('.game__cell--filled').should('have.length.greaterThan', 44)
+  })
+})
