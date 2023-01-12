89587739ab9a7597df41c54db23a2b417a83916d
Drilon Mecuku <driloni92@hotmail.com>
2023-01-12 17:42:09 +0000

    add hint test

---
+++ cypress/e2e/hint.cy.ts
@@ -0,0 +1,14 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('/')
+    cy.get('.game__cell.game__cell--filled').should('have.length', 45)
+    cy.get('.game__cell')
+        .not('.game__cell--filled')
+        .each(($cell) => {
+            cy.wrap($cell).click()
+            cy.get('.status__action-hint').click()
+    })
+
+    cy.contains('.overlay__text', 'You solved it').should('be.visible')
+  })
+})
